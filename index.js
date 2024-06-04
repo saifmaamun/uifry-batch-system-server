6d9d8f5d20b8f976eab9c4c1a7d012dbed220f17
Saif Maamun <sadifmamun@gmail.com>
2024-06-04 14:29:01 +0600

    server

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ async function run() {
         app.get('/features', async (req, res) => {
             const cursor = featuresCollection.find({});
             const features = await cursor.toArray();
+            // console.log('hitted features')
             res.send(features);
         })
         // GET testimonials API
@@ -41,81 +42,7 @@ async function run() {
             res.send(testimonials);
         })
 
-        // // for booking
-        // // GET API
-        // app.get('/orders', async (req, res) => {
-        //     const cursor = bookedTour.find({});
-        //     const booked = await cursor.toArray();
-        //     console.log("showing from booked", booked)
-        //     res.send(booked);
-        // })
 
-        // // GET A SINGEL API
-        // app.get('/destinationDetails/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     console.log('specific id',id)
-        //     const query = { _id: ObjectId(id) };
-        //     const destination = await tourCollaction.findOne(query);
-        //     res.json(destination)
-        // })
-
-        // // POST API
-        // app.post('/destinations', async (req, res) => {
-        //     const destination = req.body;
-        //     console.log('hitting', destination)
-
-        //     const result = await tourCollaction.insertOne(destination);
-        //     console.log(result)
-        //     res.json(result)
-        // })
-
-        // // for booking
-        // // POST API
-        // app.post('/orders', async (req, res) => {
-        //     const booked = req.body;
-        //     console.log('hitting', booked)
-
-        //     const result = await bookedTour.insertOne(booked);
-        //     console.log(result)
-        //     res.json(result)
-        // })
-        // // for booking
-        // //DELETE API
-        // app.delete('/booked/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: new ObjectId(id) };
-        //     const result = await bookedTour.deleteOne(query)
-        //     console.log(result)
-        //     res.json(result)
-        // })
-        // //DELETE API
-        // app.delete('/destinations/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: ObjectId(id) };
-        //     const result = await tourCollaction.deleteOne(query)
-        //     console.log(result)
-        //     res.json(result)
-        // })
-
-
-        // //UPDATE API
-        // app.put('/books/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const updatedBook = req.body;
-        //     const filter = { _id: ObjectId(id) };
-        //     const updateDoc = {
-        //         $set: {
-        //             name: updatedBook.name,
-        //             writer: updatedBook.writer,
-        //             hints: updatedBook.hints,
-        //             img: updatedBook.img,
-        //             price: updatedBook.price,
-        //         },
-        //     };
-        //     const result = await booksCollaction.updateOne(filter, updateDoc)
-        //     console.log('updating', id)
-        //     res.json(result)
-        // })
 
 
 
